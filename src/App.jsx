15437cd6c57dcba73cac54d81c0f4c9a55d3d5c6
npador07 <npador07@gmail.com>
2025-11-30 23:17:11 +0800

    deleted comment of AI

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import watchImage from "./assets/NebulaWatch.jpg"; // <-- place your image in src/assets/
+import watchImage from "./assets/NebulaWatch.jpg";
 
 function App() {
   const [open, setOpen] = useState(false);
